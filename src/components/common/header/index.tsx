ac729d4e12f70e57ea878f136273740fdefb818d
ZhongJian <zhongjian_mail@163.com>
2024-11-06 20:48:08 +0800

    featL long touch

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,6 +1,14 @@
+'use client';
+
 import logo from '@/assets/icons/logo.png';
+import { useTouch } from '@/components/display/hybrid';
+import {
+  DeveloperFlags,
+  useGlobalSettingsStore,
+} from '@/contexts/useGlobalSettingsStore';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useLongPress } from 'react-use';
 import { lusitana } from '../../display/fonts';
 import ThemeSwitcher from '../theme-switcher';
 import { AvatarDropdown } from './avatar-dropdown';
@@ -10,18 +18,45 @@ interface HeaderProps {
 }
 
 export default function Header({ isThemeShow = false }: HeaderProps) {
+  const isTouch = useTouch();
+  const developerFlags = useGlobalSettingsStore(state => state.developerFlags);
+  const setDeveloperFlags = useGlobalSettingsStore(
+    state => state.setDeveloperFlags
+  );
+
+  const longPressEvent = useLongPress(
+    (event: TouchEvent | MouseEvent) => {
+      if (isTouch || event.metaKey) {
+        console.log('toggle dev-tools after long pressing');
+        if (developerFlags) {
+          setDeveloperFlags(DeveloperFlags.NONE);
+        } else {
+          setDeveloperFlags(DeveloperFlags.NORMAL);
+        }
+      }
+    },
+    {
+      isPreventDefault: true,
+      delay: 1000,
+    }
+  );
+
   return (
     <div className="w-full flex items-center justify-between">
-      <Link href="/">
-        <div
-          className={`${lusitana.className} flex flex-row items-center gap-2 leading-none`}
-        >
+      <div
+        className={`${lusitana.className} flex flex-row items-center gap-2 leading-none`}
+      >
+        <Link href="/">
           <Image alt="logo" className="w-8 h-8 md:w-12 md:h-12" src={logo} />
-          <p className="hidden sm:block sm:text-md md:text-2xl">
-            四川鼎兴数智教育咨询有限公司
-          </p>
-        </div>
-      </Link>
+        </Link>
+        <p
+          className="hidden sm:block sm:text-md md:text-2xl hover:animate-pulse cursor-default"
+          {...longPressEvent}
+        >
+          四川鼎兴数智教育咨询有限公司
+        </p>
+      </div>
+
       <div className="flex items-center gap-2">
         {isThemeShow && <ThemeSwitcher />}
         <AvatarDropdown />
--- src/components/common/theme-switcher.tsx
+++ src/components/common/theme-switcher.tsx
@@ -4,12 +4,7 @@ import { Moon, Sun } from 'lucide-react';
 import { useEffect } from 'react';
 import useColorScheme from '../../contexts/useColorScheme';
 // import { ColorScheme } from '../../interfaces/ColorScheme';
-import { useLongPress } from 'react-use';
-import { useTouch } from '../display/hybrid';
-import {
-  DeveloperFlags,
-  useGlobalSettingsStore,
-} from '@/contexts/useGlobalSettingsStore';
+
 enum ColorScheme {
   LIGHT = 'light',
   DARK = 'dark',
@@ -17,11 +12,7 @@ enum ColorScheme {
 
 function ThemeSwitcher() {
   const { colorScheme, setColorScheme } = useColorScheme();
-  const isTouch = useTouch();
-  const developerFlags = useGlobalSettingsStore(state => state.developerFlags);
-  const setDeveloperFlags = useGlobalSettingsStore(
-    state => state.setDeveloperFlags
-  );
+
 
   useEffect(() => {
     const isDark = colorScheme !== ColorScheme.LIGHT;
@@ -41,28 +32,12 @@ function ThemeSwitcher() {
     window.location.reload();
   };
 
-  const longPressEvent = useLongPress(
-    (event: TouchEvent | MouseEvent) => {
-      if (isTouch || event.metaKey) {
-        console.log('toggle dev-tools after long pressing');
-        if (developerFlags) {
-          setDeveloperFlags(DeveloperFlags.NONE);
-        } else {
-          setDeveloperFlags(DeveloperFlags.NORMAL);
-        }
-      }
-    },
-    {
-      isPreventDefault: true,
-      delay: 1000,
-    }
-  );
+
 
   return (
     <div
-      className="cursor-pointer hover:animate-pulse"
+      className="cursor-pointer"
       onClick={onColorSchemeChange}
-      {...longPressEvent}
     >
       {colorScheme === ColorScheme.LIGHT ? (
         <Sun className="mr-2 h-6 w-6" />
