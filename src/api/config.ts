411aba5c515b7ae9c43716c6db6e34a8f6441814
ZhongJian <zhongjian_mail@163.com>
2024-11-11 22:52:40 +0800

    feat: port change

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://8.137.101.138:19080'
+    ? 'http://8.137.101.138:29090'
     : '/server-api';
 
 export const isProd = process.env.NODE_ENV === 'production';
