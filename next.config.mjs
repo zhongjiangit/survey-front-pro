ac51802ebc5c513d6b3beab93d5bb390c300d30e
wangyh <wangyh@yinhai.com>
2024-12-03 12:56:21 +0800

    feat: 后端代理

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   experimental: {
     ppr: 'incremental',
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://8.137.101.138:19080/:path*'
+      }
+    ];
+  },
 };
 
 export default nextConfig;
--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,7 @@
 export const baseUrl =
   process.env.NODE_ENV === 'production'
     ? 'http://8.137.101.138:19080'
-    : '/server-api';
+    : '/api';
+    // : '/server-api';
 
 export const isProd = process.env.NODE_ENV === 'production';
