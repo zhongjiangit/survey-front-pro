c1e1c89887cf07e5d3a3cb079144c2b007eb2d98
zhongjian <zhongjian_mail@163.com>
2024-12-10 21:31:25 +0800

    feat: system info edit

--- src/app/(survey)/system/edit/page.tsx
+++ src/app/(survey)/system/edit/page.tsx
@@ -15,19 +15,11 @@ export default function Page() {
   const currentSystem = useSurveySystemStore(state => state.currentSystem);
 
   // 使用ahooks的useRequest
-  const { data: systemList, loading: getSystemListLoading } = useRequest(
-    () => {
-      if (!currentSystem?.systemId) {
-        return Promise.reject('currentSystem is not exist');
-      }
-      return Api.getSystemListAll({
-        currentSystemId: currentSystem.systemId,
-      });
-    },
-    {
-      refreshDeps: [currentSystem?.systemId],
-    }
-  );
+  const { data: systemList, loading: getSystemListLoading } = useRequest(() => {
+    return Api.getSystemListAll({
+      currentSystemId: null,
+    });
+  });
 
   const dataSources = useMemo(() => {
     if ((systemList?.data ?? []).length > 0) {
