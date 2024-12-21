1725a024f73f0f6437a1710090048255c9f66b9c
ZhongJian <zhongjian_mail@163.com>
2024-12-21 17:45:43 +0800

    feat: delete unused code

--- src/app/(survey)/collect/manage/page.tsx
+++ src/app/(survey)/collect/manage/page.tsx
@@ -1,40 +1,7 @@
 'use client';
 
-import { useSurveyCurrentRoleStore } from '@/contexts/useSurveyRoleStore';
-import { StaffTypeEnum } from '@/types/CommonType';
-import { Tabs, TabsProps } from 'antd';
-import { useMemo } from 'react';
-import MyPublishTask from './modules/my-publish-task';
-import SubPublishTask from './modules/sub-publish-task';
-
-const CollectManage = () => {
-  const currentRole = useSurveyCurrentRoleStore(state => state.currentRole);
-
-  const items: TabsProps['items'] = useMemo(() => {
-    if (currentRole && currentRole?.staffType === StaffTypeEnum.Admin) {
-      return [
-        {
-          key: '1',
-          label: '我发布的任务',
-          children: <MyPublishTask />,
-        },
-      ];
-    } else {
-      return [
-        {
-          key: '1',
-          label: '我发布的任务',
-          children: <MyPublishTask />,
-        },
-        {
-          key: '2',
-          label: '下级发布的任务',
-          children: <SubPublishTask />,
-        },
-      ];
-    }
-  }, [currentRole]);
-  return <Tabs defaultActiveKey="1" items={items} />;
+const ManageTask = () => {
+  return <div>manage</div>;
 };
 
-export default CollectManage;
+export default ManageTask;
